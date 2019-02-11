0b316644115d2597fdd84619a30a1a6f4eb56fa4
suxiaolong3 <suxiaolong1@jd.com>
2019-02-11 15:14:04 +0800

    fix

---
+++ src/com/simplemain/astar/test.java
@@ -0,0 +1,10 @@
+package com.simplemain.astar;
+
+/**
+ * Created by suxiaolong1 on 2019/2/11 15:13
+ */
+public class test {
+    public static void main(String[] args){
+        System.out.println("test 回滚");
+    }
+}
